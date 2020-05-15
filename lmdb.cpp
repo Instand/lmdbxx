b6e94fe7a83dcf3e99b6533ff617acbfe1aa1a58
Arew <noobterr@yandex.ru>
2020-05-15 10:57:57 +0300

    Add target includes and fix dependencies

---
+++ lmdb.cpp
@@ -0,0 +1,26 @@
+#include "lmdb.hpp"
+
+#include <filesystem>
+
+cs::Lmdb::Lmdb(const std::string& path, const unsigned int flags): path_(path), flags_(flags) {
+    try {
+        env_ = environment(flags);
+
+        if (!cs::FileUtils::createPathIfNoExist(path_)) {
+            std::cout << "Could not create path for Lmdb: " << path_ << std::endl;
+        }
+    }
+    catch (const lmdb::error& error) {
+        std::cout << "Lmdb construction error: " << error.what() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cout << "Lmdb construction error: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cout << "Lmdb unknown construction error\n";
+    }
+}
+
+cs::Lmdb::~Lmdb() noexcept {
+    close();
+}
